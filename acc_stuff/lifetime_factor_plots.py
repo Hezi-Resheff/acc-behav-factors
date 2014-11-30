a3db0f7b49e0f9809886a9d16179961992d91e87
Hezi Resheff <yehezkel.resheff@mail.huji.ac.il>
2014-11-30 14:36:44 +0200

    Playing with lifetime tracks

--- acc_stuff/lifetime_factor_plots.py
+++ acc_stuff/lifetime_factor_plots.py
@@ -6,8 +6,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from feature import spherical_feature
+from factors import Factors
+
 data = pd.DataFrame.from_csv("C:\\Users\\heziresheff\\Documents\\data\\storks_acc_2012\\storks_2012_id_date_acc_behav_SAMPLE.csv", sep="\t", header=None)
 
+# calc features
+f = spherical_feature().compute(data[data.index==17940138][2].apply(lambda s: np.array(s.split(',')).astype(float)).values)
 
-
-
+# factors obj -- fit 
+factors = Factors(labels=["AF", "PF", "WLK", "STD", "SIT"]).fit(pd.DataFrame(data=f, index=data[data.index==17940138][3], columns=None))
+print(factors._factors)
