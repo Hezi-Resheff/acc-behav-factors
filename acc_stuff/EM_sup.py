35191ce3cceb1c205ddb82fceef4986888927321
heziresheff-wcomp <yehezkel.resheff@mail.huji.ac.il>
2015-01-11 00:09:35 +0200

    EM continued

--- acc_stuff/EM_sup.py
+++ acc_stuff/EM_sup.py
@@ -4,25 +4,38 @@ Model the simple_feature representation as a mixture of Gaussians per class.
 from local_settings import DATA_FOLDER
 from feature import simple_features
 
+from sklearn.cross_validation import StratifiedKFold
 from sklearn.mixture import GMM
 import pandas as pd
 import numpy as np
 import os
 
 
-class GMM_clf(object):
-    def __init__(self, nCompCls=2):
+class GMM_clf():
+    def __init__(self, nCompCls=1):
         self._ncpc = nCompCls
         self._labels = []
         self._gmms = []
 
     def fit(self, X, y): 
         self._labels = np.unique(y)
+        self._gmms = []
+        for lbl in self._labels:
+            _X = X[y == lbl]
+            _gmm = GMM(n_components=self._ncpc, covariance_type="full").fit(_X)
+            self._gmms.append(_gmm)
+        return self
 
+    def predict(self, X): 
+        out = [gmm.score(X) for gmm in self._gmms] 
+        return self._labels[np.argmax(out, 0)]
 
-    def predict(self, X): pass 
+    def score(self, X, y, k):
+        _scores = []
+        for train, test in StratifiedKFold(y, n_folds=k):
+            _scores.append((self.fit(X[train], y[train]).predict(X[test]) == y[test]).mean())
+        return _scores
 
 data = pd.DataFrame.from_csv(os.path.join(DATA_FOLDER, "storks2012", "obs.csv"),index_col=120, header= None)
-data = simple_features().compute(data.values)
-
-print(data)
+X = simple_features().compute(data.values)
+print(GMM_clf().score(X, data.index.values, 3))
